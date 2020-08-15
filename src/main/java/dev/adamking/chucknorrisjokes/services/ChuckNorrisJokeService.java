a72986a48759e11cdeb885a9708b2017fffa194e
Adam King <a.king.per@gmail.com>
2020-08-15 18:11:20 +0100

    Added ChuckNorrisJokeService. Closes #2

---
+++ src/main/java/dev/adamking/chucknorrisjokes/services/ChuckNorrisJokeService.java
@@ -0,0 +1,19 @@
+package dev.adamking.chucknorrisjokes.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ChuckNorrisJokeService implements JokeService {
+
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public ChuckNorrisJokeService() {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
+    @Override
+    public String getJoke() {
+        return this.chuckNorrisQuotes.getRandomQuote();
+    }
+}
